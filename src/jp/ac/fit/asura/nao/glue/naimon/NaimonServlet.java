0c8ba88a3132584c705714fbc72f5a077a42b95a
Yukinari Toyota <xxseyxx@gmail.com>
2009-04-25 01:12:38 +0900

    NaimonServletをmerge.

--- src/jp/ac/fit/asura/nao/glue/naimon/NaimonServlet.java
+++ src/jp/ac/fit/asura/nao/glue/naimon/NaimonServlet.java
@@ -20,9 +20,9 @@ import jp.ac.fit.asura.nao.event.VisualEventListener;
 import jp.ac.fit.asura.nao.vision.GCD;
 import jp.ac.fit.asura.nao.vision.VisualContext;
 import jp.ac.fit.asura.nao.vision.VisualCortex;
+import jp.ac.fit.asura.nao.vision.perception.BallVisualObject;
 import jp.ac.fit.asura.nao.vision.perception.BlobVision;
 import jp.ac.fit.asura.nao.vision.perception.BlobVision.Blob;
-import jp.ac.fit.asura.nao.vision.perception.BallVisualObject;
 
 import org.apache.log4j.Logger;
 
@@ -55,30 +55,32 @@ public class NaimonServlet extends HttpServlet {
 		final DataOutputStream dos = new DataOutputStream(os);
 		final Object lock = new Object();
 
-		final VisualContext vx = vc.getVisualContext();
-
 		VisualEventListener listener = new VisualEventListener() {
 			public void updateVision(VisualContext context) {
-				//実機は320x240, webotsは160x120
+				// 実機は320x240, webotsは160x120
 				byte[] gcd = context.gcdPlane;
-				//blob数
+				// blob数
 
 				try {
-					//gcdデータを流す
+					// gcdデータを流す
 					os.write(gcd);
 
-					//blob
+					// blob
 					int threshold = 5;
 					String thStr = req.getParameter("threshold");
 					if (thStr != null)
 						threshold = Integer.parseInt(thStr);
-					//blob流すぜ
-					writeBlobs(dos, GCD.cORANGE, vx.blobVision.findBlobs(GCD.cORANGE, BlobVision.MAX_BLOBS, threshold));
-					writeBlobs(dos, GCD.cCYAN, vx.blobVision.findBlobs(GCD.cCYAN, BlobVision.MAX_BLOBS, threshold));
-					writeBlobs(dos, GCD.cYELLOW, vx.blobVision.findBlobs(GCD.cYELLOW, BlobVision.MAX_BLOBS, threshold));
+					// blob流すぜ
+					writeBlobs(dos, GCD.cORANGE, context.blobVision.findBlobs(
+							GCD.cORANGE, BlobVision.MAX_BLOBS, threshold));
+					writeBlobs(dos, GCD.cCYAN, context.blobVision.findBlobs(
+							GCD.cCYAN, BlobVision.MAX_BLOBS, threshold));
+					writeBlobs(dos, GCD.cYELLOW, context.blobVision.findBlobs(
+							GCD.cYELLOW, BlobVision.MAX_BLOBS, threshold));
 
-					//ballDistance
-					dos.writeInt(((BallVisualObject)vx.get(Ball)).distance);
+					// ballDistance
+					dos
+							.writeInt(((BallVisualObject) context.get(Ball)).distance);
 
 				} catch (IOException e) {
 					log.warn("Connection closed.", e);
@@ -100,11 +102,12 @@ public class NaimonServlet extends HttpServlet {
 		return;
 	}
 
-	private void writeBlobs(DataOutputStream dos, byte c, List<Blob> list) throws IOException {
-		dos.write(c&0xFF);
+	private void writeBlobs(DataOutputStream dos, byte c, List<Blob> list)
+			throws IOException {
+		dos.write(c & 0xFF);
 		dos.writeInt(list.size());
-//		log.debug("NaimonServlet: color is " + (c&0xFF));
-//		log.debug("NaimonServlet: list.size() is " + list.size());
+		// log.debug("NaimonServlet: color is " + (c&0xFF));
+		// log.debug("NaimonServlet: list.size() is " + list.size());
 
 		for (Blob b : list) {
 			dos.writeInt(b.xmin);

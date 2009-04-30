68a43b2ba53ee7b463525b2d6a95cd4071722d61
KEIY210 <keiy.desuga.nanika@gmail.com>
2009-04-30 14:17:57 +0900

    XMLがおかしかったのを修正

--- src/jp/ac/fit/asura/nao/glue/naimon/NaimonValuesServlet.java
+++ src/jp/ac/fit/asura/nao/glue/naimon/NaimonValuesServlet.java
@@ -58,19 +58,22 @@ public class NaimonValuesServlet extends HttpServlet {
 				final String[] vosName = { "Ball", "BlueGoal", "YellowGoal" };
 				String xml;
 				xml = "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>\n";
+
+				xml += "<Values>\n";
 				for (int idx = 0; idx < vos.length; idx++) {
-					xml += "<VisualObject name=\"" + vosName[idx] + "\">\n";
-					xml += "\t<CenterX>" + vos[idx].center.x + "</CenterX>\n";
-					xml += "\t<CenterY>" + vos[idx].center.y + "</CenterY>\n";
-					xml += "\t<AngleX>" + vos[idx].angle.x + "</AngleX>\n";
-					xml += "\t<AngleY>" + vos[idx].angle.y + "</AngleY>\n";
-					xml += "\t<RobotAngleX>" + vos[idx].robotAngle.x
+					xml += "\t<VisualObject name=\"" + vosName[idx] + "\">\n";
+					xml += "\t\t<CenterX>" + vos[idx].center.x + "</CenterX>\n";
+					xml += "\t\t<CenterY>" + vos[idx].center.y + "</CenterY>\n";
+					xml += "\t\t<AngleX>" + vos[idx].angle.x + "</AngleX>\n";
+					xml += "\t\t<AngleY>" + vos[idx].angle.y + "</AngleY>\n";
+					xml += "\t\t<RobotAngleX>" + vos[idx].robotAngle.x
 							+ "</RobotAngleX>\n";
-					xml += "\t<RobotAngleY>" + vos[idx].robotAngle.y
+					xml += "\t\t<RobotAngleY>" + vos[idx].robotAngle.y
 							+ "</RobotAngleY>\n";
-					xml += "\t<Conf>" + vos[idx].confidence + "</Conf>\n";
-					xml += "</VisualObject>\n";
+					xml += "\t\t<Conf>" + vos[idx].confidence + "</Conf>\n";
+					xml += "\t</VisualObject>\n";
 				}
+				xml += "</Values>\n";
 
 				w.print(xml);
 				synchronized (lock) {

cebbf93f0e5773d23238778454100b4c77a23061
Yukinari Toyota <xxseyxx@gmail.com>
2009-04-30 13:56:44 +0900

    (naimon) VisualEventListenerによる情報更新に変更.

--- src/jp/ac/fit/asura/nao/glue/naimon/NaimonValuesServlet.java
+++ src/jp/ac/fit/asura/nao/glue/naimon/NaimonValuesServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import jp.ac.fit.asura.nao.RobotContext;
+import jp.ac.fit.asura.nao.event.VisualEventListener;
 import jp.ac.fit.asura.nao.vision.VisualContext;
 import jp.ac.fit.asura.nao.vision.VisualCortex;
 import jp.ac.fit.asura.nao.vision.perception.VisualObject;
@@ -44,34 +45,50 @@ public class NaimonValuesServlet extends HttpServlet {
 		log.info("process XML request");
 		resp.setStatus(HttpServletResponse.SC_OK);
 		resp.setContentType("text/xml");
-		final VisualCortex vc = robotContext.getVision();
 
 		final PrintWriter w = resp.getWriter();
 
 		// 手始めにVisualObjectをXML形式に変換
-		VisualContext vct = vc.getVisualContext();
-		final VisualObject[] vos = { vct.get(Ball), vct.get(BlueGoal),
-				vct.get(YellowGoal) };
-		final String[] vosName = { "Ball", "BlueGoal", "YellowGoal" };
-		String xml;
-		xml = "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>\n";
-		for (int idx = 0; idx < vos.length; idx++) {
-			xml += "<VisualObject name=\"" + vosName[idx] + "\">\n";
-			xml += "\t<CenterX>" + vos[idx].center.x + "</CenterX>\n";
-			xml += "\t<CenterY>" + vos[idx].center.y + "</CenterY>\n";
-			xml += "\t<AngleX>" + vos[idx].angle.x + "</AngleX>\n";
-			xml += "\t<AngleY>" + vos[idx].angle.y + "</AngleY>\n";
-			xml += "\t<RobotAngleX>" + vos[idx].robotAngle.x
-					+ "</RobotAngleX>\n";
-			xml += "\t<RobotAngleY>" + vos[idx].robotAngle.y
-					+ "</RobotAngleY>\n";
-			xml += "\t<Conf>" + vos[idx].confidence + "</Conf>\n";
-			xml += "</VisualObject>\n";
+		final Object lock = new Object();
+
+		VisualEventListener listener = new VisualEventListener() {
+			public void updateVision(VisualContext context) {
+				final VisualObject[] vos = { context.get(Ball),
+						context.get(BlueGoal), context.get(YellowGoal) };
+				final String[] vosName = { "Ball", "BlueGoal", "YellowGoal" };
+				String xml;
+				xml = "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>\n";
+				for (int idx = 0; idx < vos.length; idx++) {
+					xml += "<VisualObject name=\"" + vosName[idx] + "\">\n";
+					xml += "\t<CenterX>" + vos[idx].center.x + "</CenterX>\n";
+					xml += "\t<CenterY>" + vos[idx].center.y + "</CenterY>\n";
+					xml += "\t<AngleX>" + vos[idx].angle.x + "</AngleX>\n";
+					xml += "\t<AngleY>" + vos[idx].angle.y + "</AngleY>\n";
+					xml += "\t<RobotAngleX>" + vos[idx].robotAngle.x
+							+ "</RobotAngleX>\n";
+					xml += "\t<RobotAngleY>" + vos[idx].robotAngle.y
+							+ "</RobotAngleY>\n";
+					xml += "\t<Conf>" + vos[idx].confidence + "</Conf>\n";
+					xml += "</VisualObject>\n";
+				}
+
+				w.print(xml);
+				synchronized (lock) {
+					lock.notifyAll();
+				}
+			}
+		};
+
+		VisualCortex vc = robotContext.getVision();
+		try {
+			vc.addEventListener(listener);
+			synchronized (lock) {
+				lock.wait();
+			}
+		} catch (InterruptedException e) {
+		} finally {
+			vc.removeEventListener(listener);
 		}
-
-		w.print(xml);
-
 		return;
 	}
-
 }

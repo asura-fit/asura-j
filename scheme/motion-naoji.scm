1e4a64e17dec2d88cf971a4ed3c8f98203c729b1
Yukinari Toyota <xxseyxx@gmail.com>
2009-05-07 17:22:03 +0900

    Disable walkArms

--- scheme/motion-naoji.scm
+++ scheme/motion-naoji.scm
@@ -2,4 +2,6 @@
 (define naojiwalker (NaojiWalker. jalmotion 30))
 (.setWalkConfig jalmotion 0.05f 0.008f 0.06f 0.3f 0.015f 0.018f)
 (.setWalkExtraConfig jalmotion 3.5f -3.5f 0.215f 3.0f)
+; 腕を使用するか?
+(.setWalkArmsEnable jalmotion #f)
 (mc-registmotion2 80 naojiwalker)

9a4475a7ced2bff2f2269f9d5d241762242ac9a9
Yukinari Toyota <xxseyxx@gmail.com>
2009-06-19 18:41:05 +0900

    Webots用の設定ファイルのサンプルを追加.

---
+++ scheme/autoexec.sample.webots.scm
@@ -0,0 +1,30 @@
+
+(start-httpd 8080)
+
+;(loglevel "jp.ac.fit.asura.nao.communication" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.misc.Kinematics" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.motion" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.motion.parameterized" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.motion.MotorCortex" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.vision.perception" INFO)
+;(loglevel "jp.ac.fit.asura.nao.vision.perception" TRACE)
+;(loglevel "jp.ac.fit.asura.nao.vision" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.sensation" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.strategy" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.localization.self" INFO)
+;(loglevel "jp.ac.fit.asura.nao.naoji.motion" DEBUG)
+;(loglevel "jp.ac.fit.asura.nao.naoji.NaojiPlayer" TRACE)
+;(loglevel "jp.ac.fit.asura.nao.naoji.NaojiDriver" TRACE)
+
+;(ss-set-team "Red")
+;(ss-set-role "Striker")
+;(ss-set-role "Goalie")
+
+;(ss-scheduler "ExperimentalScheduler")
+
+(mc-motorpower 1.0f)
+
+; nao_robocup_webots6/controllers/nao_soccer_player_redに色切りファイル
+; http://asura.fit.ac.jp/private/hg/utilities-nao/raw-file/tip/colorTable/robotstadium2009/normal.tm2
+; を入れること
+(vc-load-tmap "normal.tm2")

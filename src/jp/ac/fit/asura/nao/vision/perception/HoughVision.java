22e5d39b9367dea90c523af9263df88f2027c88e
Yukinari Toyota <xxseyxx@gmail.com>
2009-10-30 13:41:18 +0900

    Parameters of hough transform was modified.

--- src/jp/ac/fit/asura/nao/vision/perception/HoughVision.java
+++ src/jp/ac/fit/asura/nao/vision/perception/HoughVision.java
@@ -9,12 +9,14 @@ import jp.ac.fit.asura.nao.misc.MathUtils;
 import jp.ac.fit.asura.nao.vision.GCD;
 
 /**
+ * ハフ変換による直線検出
+ *
  * @author sey
  *
  */
 public class HoughVision extends AbstractVision {
-	public static final int THETA_MAX = 72;
-	public static final int RHO_SCALE = 2;
+	public static final int THETA_MAX = 360 / 6;
+	public static final int RHO_SCALE = 4;
 	public static final int RHO_MAX = 160 / RHO_SCALE;
 	private byte[] th_rho;
 	private float[] c;
@@ -33,31 +35,36 @@ public class HoughVision extends AbstractVision {
 	public void process() {
 		getContext().houghPlane = th_rho;
 		byte[] plane = getContext().gcdPlane;
+		houghTransform(plane, th_rho);
+	}
+
+	private void houghTransform(byte[] src, byte[] dest) {
 		int width = getContext().image.getWidth();
 		int height = getContext().image.getHeight();
 
-		Arrays.fill(th_rho, (byte) 0);
+		Arrays.fill(dest, (byte) 0);
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				int i = y * width + x;
 
-				if (plane[i] == GCD.cWHITE) {
+				if (src[i] == GCD.cWHITE) {
 					for (int th = 0; th < THETA_MAX; th++) {
 						int rho = (int) (x * c[th] + y * s[th]) / RHO_SCALE;
-						if (th_rho[th * RHO_MAX + rho] != -1)
-							th_rho[th * RHO_MAX + rho]++;
+						if (dest[th * RHO_MAX + rho] != -1)
+							dest[th * RHO_MAX + rho]++;
 					}
 				}
 			}
 		}
 
-//		for (int th = 0; th < THETA_MAX; th++) {
-//			for (int rho = 0; rho < RHO_MAX; rho++) {
-//				if ((th_rho[th * RHO_MAX + rho] & 0xFF) > 128)
-//					System.out.println("th:" + (th * (360 / THETA_MAX))
-//							+ " rho:" + rho + " v:"
-//							+ (th_rho[th * RHO_MAX + rho] & 0xFF));
-//			}
-//		}
+		// for (int th = 0; th < THETA_MAX; th++) {
+		// for (int rho = 0; rho < RHO_MAX; rho++) {
+		// if ((th_rho[th * RHO_MAX + rho] & 0xFF) > 128)
+		// System.out.println("th:" + (th * (360 / THETA_MAX))
+		// + " rho:" + rho + " v:"
+		// + (th_rho[th * RHO_MAX + rho] & 0xFF));
+		// }
+		// }
+
 	}
 }

ec2251776996dbcbc149d6d73d4ea2887d492179
takata <devnull@localhost>
2010-04-28 13:16:17 +0900

    2010ルールに合わせてphysical/Ball, Fieldを変更.

--- src/jp/ac/fit/asura/nao/physical/Ball.java
+++ src/jp/ac/fit/asura/nao/physical/Ball.java
@@ -5,11 +5,11 @@ package jp.ac.fit.asura.nao.physical;
 
 /**
  * @author sey
- * 
+ *
  * @version $Id: Ball.java 691 2008-09-26 06:40:26Z sey $
- * 
+ *
  */
 public class Ball {
 	// mm
-	public static final int Radius = 43;
+	public static final float Radius = 32.5f;
 }

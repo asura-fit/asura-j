77d4f9671291584424fcd852fe4f62410d3cfcbf
kobayashi <devnull@localhost>
2011-04-16 16:11:35 +0900

    歩き調整の追加(62はやってない)

--- scheme/motion-naoji.scm
+++ scheme/motion-naoji.scm
@@ -4,6 +4,7 @@
 (define (set-walk-jointpower joint value) (.setJointStiffness naojiwalker joint value))
 (define (set-walk-samples samples) (.setWalkSamples naojiwalker samples))
 
+
 ; setWalkConfigの値が使えないと標準出力にエラーがでる
 ; 去年のやつをちょっとかえたもの
 ;(.setWalkConfig jalmotion 0.055f 0.02f 0.025f 0.25f 0.22f 3.3f)
@@ -12,6 +13,11 @@
 (.setWalkTrapezoidConfig jalmotion 5.0f -5.0f)
 ; モノセン赤(2011/03/09)
 (.setWalkConfig jalmotion 0.035f 0.015f 0.025f 0.20f 0.22f 3.3f)
+;資料館 63(2011/04/15)
+;(.setWalkConfig jalmotion 0.055f 0.01f 0.025f 0.25f 0.22f 3.0f)
+;資料館 61(2011/04/15)
+;(.setWalkConfig jalmotion 0.055f 0.02f 0.025f 0.25f 0.22f 3.3f)
+
 
 ; 腕を使用するか?
 (.setWalkArmsEnable jalmotion #t)
--- scheme/motion-walkConfig1.scm
+++ scheme/motion-walkConfig1.scm
@@ -1,4 +1,4 @@
-; 192.168.1.54/64
+; 192.168.1.51/61
 
 ; 歩く時の関節のStiffnessを設定
 (set-walk-jointpower "RHipPitch" 0.8f)
@@ -7,8 +7,8 @@
 (set-walk-jointpower "LHipYawPitch" 0.6f)
 (set-walk-jointpower "RHipRoll" 0.7f)
 (set-walk-jointpower "LHipRoll" 0.7f)
-(set-walk-jointpower "RAnkleRoll" 0.9f)
-(set-walk-jointpower "LAnkleRoll" 0.9f)
+(set-walk-jointpower "RAnkleRoll" 0.8f)
+(set-walk-jointpower "LAnkleRoll" 0.8f)
 (set-walk-jointpower "RKneePitch" 0.7f)
 (set-walk-jointpower "LKneePitch" 0.7f)
 (set-walk-jointpower "RAnklePitch" 0.5f)

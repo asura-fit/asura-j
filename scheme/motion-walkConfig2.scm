f152606cd33dfe1a62790c13fcbd079a9fa9bdd7
asura-fit <devnull@localhost>
2012-04-19 18:19:38 +0900

    B4の歩き調整62/64

--- scheme/motion-walkConfig2.scm
+++ scheme/motion-walkConfig2.scm
@@ -28,21 +28,40 @@
 ;(set-walk-jointpower "RAnklePitch" 0.8f)
 ;(set-walk-jointpower "LAnklePitch" 0.8f)
 ;  修練道場
+;(set-walk-jointpower "RHipPitch" 0.8f)
+;(set-walk-jointpower "LHipPitch" 0.8f)
+;(set-walk-jointpower "RHipYawPitch" 0.7f)
+;(set-walk-jointpower "LHipYawPitch" 0.7f)
+;(set-walk-jointpower "RHipRoll" 0.83f)
+;(set-walk-jointpower "LHipRoll" 0.83f)
+;(set-walk-jointpower "RAnkleRoll" 0.9f)
+;(set-walk-jointpower "LAnkleRoll" 0.9f)
+;(set-walk-jointpower "RKneePitch" 0.7f)
+;(set-walk-jointpower "LKneePitch" 0.78f)
+;(set-walk-jointpower "RAnklePitch" 0.8f)
+;(set-walk-jointpower "LAnklePitch" 0.86f)
+
+
+;B4階
 (set-walk-jointpower "RHipPitch" 0.8f)
 (set-walk-jointpower "LHipPitch" 0.8f)
 (set-walk-jointpower "RHipYawPitch" 0.7f)
 (set-walk-jointpower "LHipYawPitch" 0.7f)
 (set-walk-jointpower "RHipRoll" 0.83f)
 (set-walk-jointpower "LHipRoll" 0.83f)
-(set-walk-jointpower "RAnkleRoll" 0.9f)
+(set-walk-jointpower "RAnkleRoll" 0.8f)
 (set-walk-jointpower "LAnkleRoll" 0.9f)
 (set-walk-jointpower "RKneePitch" 0.7f)
 (set-walk-jointpower "LKneePitch" 0.78f)
 (set-walk-jointpower "RAnklePitch" 0.8f)
 (set-walk-jointpower "LAnklePitch" 0.86f)
 
+
+;b4階
+(.setWalkConfig jalmotion 0.02f 0.016f 0.015f 0.23f 0.22f 0.98f)
+(.setWalkTrapezoidConfig jalmotion 8.0f -8.0f)
 ;修練道場 52/62
-(.setWalkConfig jalmotion 0.03f 0.013f 0.015f 0.23f 0.22f -0.98f)
-(.setWalkTrapezoidConfig jalmotion 6.0f -6.0f)
+;(.setWalkConfig jalmotion 0.03f 0.013f 0.015f 0.23f 0.22f -0.98f)
+;(.setWalkTrapezoidConfig jalmotion 6.0f -6.0f)
 ; Walkのパラメータ samplesの設定
 (set-walk-samples 40)
--- scheme/motion-walkConfig4.scm
+++ scheme/motion-walkConfig4.scm
@@ -14,24 +14,40 @@
 ;(set-walk-jointpower "RAnklePitch" 0.5f)
 ;(set-walk-jointpower "LAnklePitch" 0.5f)
 
-(set-walk-jointpower "RHipPitch" 0.8f)
-(set-walk-jointpower "LHipPitch" 0.8f)
+;(set-walk-jointpower "RHipPitch" 0.8f)
+;(set-walk-jointpower "LHipPitch" 0.8f)
+;(set-walk-jointpower "RHipYawPitch" 0.6f)
+;(set-walk-jointpower "LHipYawPitch" 0.6f)
+;(set-walk-jointpower "RHipRoll" 0.8f)
+;(set-walk-jointpower "LHipRoll" 0.8f)
+;(set-walk-jointpower "RAnkleRoll" 0.8f)
+;(set-walk-jointpower "LAnkleRoll" 0.8f)
+;(set-walk-jointpower "RKneePitch" 0.6f)
+;(set-walk-jointpower "LKneePitch" 0.7f)
+;(set-walk-jointpower "RAnklePitch" 0.8f)
+;(set-walk-jointpower "LAnklePitch" 0.8f)
+
+;B4
+(set-walk-jointpower "RHipPitch" 0.7f)
+(set-walk-jointpower "LHipPitch" 0.7f)
 (set-walk-jointpower "RHipYawPitch" 0.6f)
 (set-walk-jointpower "LHipYawPitch" 0.6f)
-(set-walk-jointpower "RHipRoll" 0.8f)
+(set-walk-jointpower "RHipRoll" 0.7)
 (set-walk-jointpower "LHipRoll" 0.8f)
 (set-walk-jointpower "RAnkleRoll" 0.8f)
-(set-walk-jointpower "LAnkleRoll" 0.8f)
+(set-walk-jointpower "LAnkleRoll" 0.9f)
 (set-walk-jointpower "RKneePitch" 0.6f)
-(set-walk-jointpower "LKneePitch" 0.7f)
-(set-walk-jointpower "RAnklePitch" 0.8f)
-(set-walk-jointpower "LAnklePitch" 0.8f)
+(set-walk-jointpower "LKneePitch" 0.6f)
+(set-walk-jointpower "RAnklePitch" 0.6f)
+(set-walk-jointpower "LAnklePitch" 0.6f)
 
-
+;B4
+(.setWalkConfig jalmotion 0.017f 0.014f 0.025f 0.4f 0.224f 3.2f)
+;(.setWalkTrapezoidConfig jalmotion 6.0f -3.0f)
 
 ;修練道場naojiターン54/64
-(.setWalkConfig jalmotion 0.015f 0.01f 0.025f 0.4f 0.23f 3.0f)
-(.setWalkTrapezoidConfig jalmotion 4.0f -4.0f)
+;(.setWalkConfig jalmotion 0.015f 0.01f 0.025f 0.4f 0.23f 3.0f)
+;(.setWalkTrapezoidConfig jalmotion 4.0f -4.0f)
 		;修練道場赤い子
 		;samples = 33;
 		;angle = 0.245f;

6125819163e9c0a2e9b64e9ce834afa060030230
ito <devnull@localhost>
2012-04-19 18:31:41 +0900

    b4noaruki61to63

--- scheme/motion-walkConfig1.scm
+++ scheme/motion-walkConfig1.scm
@@ -30,6 +30,20 @@
 ;(set-walk-jointpower "RAnklePitch" 0.5f)
 ;(set-walk-jointpower "LAnklePitch" 0.5f)
 
+;(set-walk-jointpower "RHipPitch" 0.6f)
+;(set-walk-jointpower "LHipPitch" 0.6f)
+;(set-walk-jointpower "RHipYawPitch" 0.6f)
+;(set-walk-jointpower "LHipYawPitch" 0.6f)
+;(set-walk-jointpower "RHipRoll" 0.7f)
+;(set-walk-jointpower "LHipRoll" 0.7f)
+;(set-walk-jointpower "RAnkleRoll" 0.7f)
+;(set-walk-jointpower "LAnkleRoll" 0.7f)
+;(set-walk-jointpower "RKneePitch" 0.7f)
+;(set-walk-jointpower "LKneePitch" 0.7f)
+;(set-walk-jointpower "RAnklePitch" 0.5f)
+;(set-walk-jointpower "LAnklePitch" 0.5f)
+
+;b4
 (set-walk-jointpower "RHipPitch" 0.6f)
 (set-walk-jointpower "LHipPitch" 0.6f)
 (set-walk-jointpower "RHipYawPitch" 0.6f)
@@ -42,11 +56,12 @@
 (set-walk-jointpower "LKneePitch" 0.7f)
 (set-walk-jointpower "RAnklePitch" 0.5f)
 (set-walk-jointpower "LAnklePitch" 0.5f)
-
-
+;b4
+(.setWalkConfig jalmotion 0.02f 0.01f 0.025f 0.4f 0.24f 5.0f)
+(.setWalkTrapezoidConfig jalmotion 4.0f -4.0f)
 ;修練道場51/61改
-(.setWalkConfig jalmotion 0.02f 0.01f 0.025f 0.4f 0.14f 5.0f)
-(.setWalkTrapezoidConfig jalmotion 5.0f -5.0f)
+;(.setWalkConfig jalmotion 0.02f 0.01f 0.025f 0.4f 0.14f 5.0f)
+;(.setWalkTrapezoidConfig jalmotion 5.0f -5.0f)
 
 
 ; Walkのパラメータ samplesの設定
--- scheme/motion-walkConfig3.scm
+++ scheme/motion-walkConfig3.scm
@@ -44,27 +44,40 @@
 
 
 ;修練道場改
+;(set-walk-jointpower "RHipPitch" 0.7f)
+;(set-walk-jointpower "LHipPitch" 0.7f)
+;(set-walk-jointpower "RHipYawPitch" 0.7f)
+;(set-walk-jointpower "LHipYawPitch" 0.7f)
+;(set-walk-jointpower "RHipRoll" 0.7f)
+;(set-walk-jointpower "LHipRoll" 0.7f)
+;(set-walk-jointpower "RAnkleRoll" 0.6f)
+;(set-walk-jointpower "LAnkleRoll" 0.6f)
+;(set-walk-jointpower "RKneePitch" 0.7f)
+;(set-walk-jointpower "LKneePitch" 0.7f)
+;(set-walk-jointpower "RAnklePitch" 0.6f)
+;(set-walk-jointpower "LAnklePitch" 0.6f)
+
+;修練道場53/63改
+;(.setWalkConfig jalmotion 0.015f 0.02f 0.025f 0.3f 0.214f 6.0f)
+;(.setWalkTrapezoidConfig jalmotion 5.0f -2.0f)
+
+;b4
 (set-walk-jointpower "RHipPitch" 0.7f)
 (set-walk-jointpower "LHipPitch" 0.7f)
-(set-walk-jointpower "RHipYawPitch" 0.7f)
-(set-walk-jointpower "LHipYawPitch" 0.7f)
-(set-walk-jointpower "RHipRoll" 0.7f)
-(set-walk-jointpower "LHipRoll" 0.7f)
-(set-walk-jointpower "RAnkleRoll" 0.6f)
+(set-walk-jointpower "RHipYawPitch" 0.6f)
+(set-walk-jointpower "LHipYawPitch" 0.6f)
+(set-walk-jointpower "RHipRoll" 0.65f)
+(set-walk-jointpower "LHipRoll" 0.65f)
+(set-walk-jointpower "RAnkleRoll" 0.5f)
 (set-walk-jointpower "LAnkleRoll" 0.6f)
 (set-walk-jointpower "RKneePitch" 0.7f)
 (set-walk-jointpower "LKneePitch" 0.7f)
 (set-walk-jointpower "RAnklePitch" 0.6f)
 (set-walk-jointpower "LAnklePitch" 0.6f)
 
-;修練道場53/63改
-(.setWalkConfig jalmotion 0.015f 0.02f 0.025f 0.3f 0.214f 6.0f)
-(.setWalkTrapezoidConfig jalmotion 5.0f -2.0f)
-
-
-
-
-
+;b4
+(.setWalkConfig jalmotion 0.0168f 0.018f 0.024f 0.3f 0.214f 6.0f)
+(.setWalkTrapezoidConfig jalmotion 6.9f -3.4f)
 
 ; Walkのパラメータ samplesの設定
-(set-walk-samples 25)
+(set-walk-samples 27)

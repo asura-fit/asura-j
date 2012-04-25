d02cb268b2309755a4cc471800baca0084055753
notti <devnull@localhost>
2012-04-25 19:41:48 +0900

    赤い子の歩き調整byKBYS

--- scheme/motion-walkConfig4.scm
+++ scheme/motion-walkConfig4.scm
@@ -29,20 +29,23 @@
 
 ;B4
 (set-walk-jointpower "RHipPitch" 0.7f)
-(set-walk-jointpower "LHipPitch" 0.7f)
-(set-walk-jointpower "RHipYawPitch" 0.6f)
+(set-walk-jointpower "LHipPitch" 0.6f)
+(set-walk-jointpower "RHipYawPitch" 0.7f)
 (set-walk-jointpower "LHipYawPitch" 0.6f)
-(set-walk-jointpower "RHipRoll" 0.7)
-(set-walk-jointpower "LHipRoll" 0.8f)
-(set-walk-jointpower "RAnkleRoll" 0.8f)
-(set-walk-jointpower "LAnkleRoll" 0.9f)
+(set-walk-jointpower "RHipRoll" 0.6)
+(set-walk-jointpower "LHipRoll" 0.6f)
+(set-walk-jointpower "RAnkleRoll" 0.7f)
+(set-walk-jointpower "LAnkleRoll" 0.7f)
 (set-walk-jointpower "RKneePitch" 0.6f)
-(set-walk-jointpower "LKneePitch" 0.6f)
-(set-walk-jointpower "RAnklePitch" 0.6f)
-(set-walk-jointpower "LAnklePitch" 0.6f)
+(set-walk-jointpower "LKneePitch" 0.5f)
+(set-walk-jointpower "RAnklePitch" 0.7f)
+(set-walk-jointpower "LAnklePitch" 0.7f)
+
+
+
 
 ;B4
-(.setWalkConfig jalmotion 0.017f 0.014f 0.025f 0.4f 0.224f 3.2f)
+(.setWalkConfig jalmotion 0.035f 0.018f 0.015f 0.4f 0.224f 3.3f)
 ;(.setWalkTrapezoidConfig jalmotion 6.0f -3.0f)
 
 ;修練道場naojiターン54/64
@@ -54,7 +57,7 @@
 		;sideDist = 0.04f;
 
 ; Walkのパラメータ samplesの設定
-(set-walk-samples 20)
+(set-walk-samples 25)
 
 
 ;赤たん
@@ -63,7 +66,7 @@
 ;(.setWalkConfig jalmotion 0.035f 0.017f 0.02f 0.4f 0.224f 3.3f)
 
 
-(.setWalkTrapezoidConfig jalmotion 9.0f -9.0f)
+;(.setWalkTrapezoidConfig jalmotion 9.0f -9.0f)
 (set-circleturn-angle 0.25f)
 (set-circleturn-sidedist 0.045f)
 

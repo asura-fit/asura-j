4ef0e0aa6ad1565a6ca64a4c9f0fda74ac545fce
asura-fit <devnull@localhost>
2012-04-28 19:02:04 +0900

    赤調整

--- scheme/motion-walkConfig4.scm
+++ scheme/motion-walkConfig4.scm
@@ -43,22 +43,7 @@
 
 
 
-;(.setWalkConfig jalmotion 0.03f 0.02f 0.015f 0.4f 0.224f 1.0f)
-
-;(set-walk-jointpower "RHipPitch" 0.7f)
-;(set-walk-jointpower "LHipPitch" 0.6f)
-;(set-walk-jointpower "RHipYawPitch" 0.7f)
-;(set-walk-jointpower "LHipYawPitch" 0.6f)
-;(set-walk-jointpower "RHipRoll" 0.6f)
-;(set-walk-jointpower "LHipRoll" 0.6f)
-;(set-walk-jointpower "RAnkleRoll" 0.7f)
-;(set-walk-jointpower "LAnkleRoll" 0.7f)
-;(set-walk-jointpower "RKneePitch" 0.6f)
-;(set-walk-jointpower "LKneePitch" 0.5f)
-;(set-walk-jointpower "RAnklePitch" 0.7f)
-;(set-walk-jointpower "LAnklePitch" 0.7f)
-
-(.setWalkConfig jalmotion 0.02f 0.026f 0.02f 0.4f 0.24f 1.0f)
+(.setWalkConfig jalmotion 0.023f 0.02f 0.015f 0.4f 0.236f 3.0f)
 
 (set-walk-jointpower "RHipPitch" 0.7f)
 (set-walk-jointpower "LHipPitch" 0.6f)
@@ -66,12 +51,14 @@
 (set-walk-jointpower "LHipYawPitch" 0.6f)
 (set-walk-jointpower "RHipRoll" 0.6f)
 (set-walk-jointpower "LHipRoll" 0.6f)
-(set-walk-jointpower "RAnkleRoll" 0.6f)
-(set-walk-jointpower "LAnkleRoll" 0.5f)
+(set-walk-jointpower "RAnkleRoll" 0.7f)
+(set-walk-jointpower "LAnkleRoll" 0.7f)
 (set-walk-jointpower "RKneePitch" 0.6f)
-(set-walk-jointpower "LKneePitch" 0.6f)
-(set-walk-jointpower "RAnklePitch" 0.6f)
-(set-walk-jointpower "LAnklePitch" 0.6f)
+(set-walk-jointpower "LKneePitch" 0.5f)
+(set-walk-jointpower "RAnklePitch" 0.7f)
+(set-walk-jointpower "LAnklePitch" 0.7f)
+
+
 
 
 

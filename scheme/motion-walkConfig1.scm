16e938e62098bbbeb34b260e560255194273afb7
Owner <devnull@localhost>
2012-04-30 02:23:18 +0900

    WalkConfig is changed for 51/61

--- scheme/motion-walkConfig1.scm
+++ scheme/motion-walkConfig1.scm
@@ -30,24 +30,40 @@
 ;(set-walk-jointpower "RAnklePitch" 0.5f)
 ;(set-walk-jointpower "LAnklePitch" 0.5f)
 
-(set-walk-jointpower "RHipPitch" 0.6f)
-(set-walk-jointpower "LHipPitch" 0.6f)
+;(set-walk-jointpower "RHipPitch" 0.6f)
+;(set-walk-jointpower "LHipPitch" 0.6f)
+;(set-walk-jointpower "RHipYawPitch" 0.6f)
+;(set-walk-jointpower "LHipYawPitch" 0.6f)
+;(set-walk-jointpower "RHipRoll" 0.7f)
+;(set-walk-jointpower "LHipRoll" 0.7f)
+;(set-walk-jointpower "RAnkleRoll" 0.7f)
+;(set-walk-jointpower "LAnkleRoll" 0.7f)
+;(set-walk-jointpower "RKneePitch" 0.7f)
+;(set-walk-jointpower "LKneePitch" 0.7f)
+;(set-walk-jointpower "RAnklePitch" 0.5f)
+;(set-walk-jointpower "LAnklePitch" 0.5f)
+
+(set-walk-jointpower "RHipPitch" 0.7f)
+(set-walk-jointpower "LHipPitch" 0.7f)
 (set-walk-jointpower "RHipYawPitch" 0.6f)
 (set-walk-jointpower "LHipYawPitch" 0.6f)
-(set-walk-jointpower "RHipRoll" 0.7f)
-(set-walk-jointpower "LHipRoll" 0.7f)
-(set-walk-jointpower "RAnkleRoll" 0.7f)
-(set-walk-jointpower "LAnkleRoll" 0.7f)
+(set-walk-jointpower "RHipRoll" 0.65f)
+(set-walk-jointpower "LHipRoll" 0.65f)
+(set-walk-jointpower "RAnkleRoll" 0.5f)
+(set-walk-jointpower "LAnkleRoll" 0.6f)
 (set-walk-jointpower "RKneePitch" 0.7f)
 (set-walk-jointpower "LKneePitch" 0.7f)
-(set-walk-jointpower "RAnklePitch" 0.5f)
-(set-walk-jointpower "LAnklePitch" 0.5f)
+(set-walk-jointpower "RAnklePitch" 0.6f)
+(set-walk-jointpower "LAnklePitch" 0.6f)
 
 
 ;修練道場51/61改
-(set-walk-config 0.02f 0.01f 0.025f 0.4f 0.24f 5.0f)
-(set-walk-trapezoidConfig 5.0f -5.0f)
+;(set-walk-config 0.02f 0.01f 0.025f 0.4f 0.24f 5.0f)
+;(set-walk-trapezoidConfig jalmotion 5.0f -5.0f)
 
+
+(set-walk-config 0.02f 0.017f 0.026f 0.4f 0.223f 4.8f)
+(set-walk-trapezoidConfig 3.0f -3.0f)
 
 (.set-turn-Config jalmotion 0.02f 0.01f 0.02f 0.4f 0.24f 5.0f)
 
@@ -65,7 +81,7 @@
 (set-circleturn-jointpower "LAnklePitch" 0.6f)
 
 ; Walkのパラメータ samplesの設定
-(set-walk-samples 20)
+(set-walk-samples 22)
 
 ; NaojiCircleTurnの設定
 ;(set-circleturn-samples 30)

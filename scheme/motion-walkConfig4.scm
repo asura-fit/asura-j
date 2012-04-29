8d3374b6c59a468ead0e5fba56671b268a77e72f
kobayashi <devnull@localhost>
2012-04-29 17:21:01 +0900

    ６４調整

--- scheme/motion-walkConfig4.scm
+++ scheme/motion-walkConfig4.scm
@@ -43,20 +43,39 @@
 
 
 
-(set-walk-config 0.023f 0.02f 0.015f 0.4f 0.236f 3.0f)
+;(set-walk-config 0.023f 0.02f 0.015f 0.4f 0.236f 3.0f)
+
+;(set-walk-jointpower "RHipPitch" 0.7f)
+;(set-walk-jointpower "LHipPitch" 0.6f)
+;(set-walk-jointpower "RHipYawPitch" 0.7f)
+;(set-walk-jointpower "LHipYawPitch" 0.6f)
+;(set-walk-jointpower "RHipRoll" 0.6f)
+;(set-walk-jointpower "LHipRoll" 0.6f)
+;(set-walk-jointpower "RAnkleRoll" 0.7f)
+;(set-walk-jointpower "LAnkleRoll" 0.7f)
+;(set-walk-jointpower "RKneePitch" 0.6f)
+;(set-walk-jointpower "LKneePitch" 0.5f)
+;(set-walk-jointpower "RAnklePitch" 0.7f)
+;(set-walk-jointpower "LAnklePitch" 0.7f)
+
+
+(set-walk-config 0.02f 0.017f 0.015f 0.4f 0.236f 3.0f)
+(set-walk-trapezoidConfig 3.5f -3.5f)
+
 
 (set-walk-jointpower "RHipPitch" 0.7f)
-(set-walk-jointpower "LHipPitch" 0.6f)
-(set-walk-jointpower "RHipYawPitch" 0.7f)
+(set-walk-jointpower "LHipPitch" 0.7f)
+(set-walk-jointpower "RHipYawPitch" 0.6f)
 (set-walk-jointpower "LHipYawPitch" 0.6f)
-(set-walk-jointpower "RHipRoll" 0.6f)
-(set-walk-jointpower "LHipRoll" 0.6f)
+(set-walk-jointpower "RHipRoll" 0.7f)
+(set-walk-jointpower "LHipRoll" 0.8f)
 (set-walk-jointpower "RAnkleRoll" 0.7f)
 (set-walk-jointpower "LAnkleRoll" 0.7f)
-(set-walk-jointpower "RKneePitch" 0.6f)
-(set-walk-jointpower "LKneePitch" 0.5f)
-(set-walk-jointpower "RAnklePitch" 0.7f)
-(set-walk-jointpower "LAnklePitch" 0.7f)
+(set-walk-jointpower "RKneePitch" 0.7f)
+(set-walk-jointpower "LKneePitch" 0.7f)
+(set-walk-jointpower "RAnklePitch" 0.8f)
+(set-walk-jointpower "LAnklePitch" 0.8f)
+
 
 
 

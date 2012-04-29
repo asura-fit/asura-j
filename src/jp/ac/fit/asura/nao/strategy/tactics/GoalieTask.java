3ad01d5c1a78e8c4d5f90db42613b80029283022
aqua <devnull@localhost>
2012-04-29 22:29:52 +0900

    ゴーリータスクの更新。log削除他

--- src/jp/ac/fit/asura/nao/strategy/tactics/GoalieTask.java
+++ src/jp/ac/fit/asura/nao/strategy/tactics/GoalieTask.java
@@ -37,30 +37,32 @@ public class GoalieTask extends Task {
 	public void init(RobotContext context) {
 		tracking = (BallTrackingTask) context.getStrategy().getTaskManager()
 				.find("BallTracking");
-		balld = 0;
-		balld0 = 0;
-		ballh = 0;
-		ballh0 = 0;
 
 	}
 
 	public void after(StrategyContext context) {
 
+//		log.debug("balld(after) =" + balld);
+//		log.debug("balld0(after)=" + balld0);
+//		log.debug("ballh(after) =" + ballh);
+//		log.debug("ballh0(after)=" + ballh0);
+//		log.info("after");
+
 		if (ballc != 0) {
-			if (context.getGoalieFlag() == false
-					&& context.getFrame() % 10 == 0) {
+			if (context.getGoalieFlag() == false && context.getFrame() % 5 == 0) {
 
 				// 前のボールの値を保存する
 				balld0 = balld;
 				ballh0 = ballh;
 
+//				log.debug("balld(after2)=" + balld);
+//				log.debug("balld0(after2)=" + balld0);
+//				log.debug("ballh(after2)=" + ballh);
+//				log.debug("ballh0(after2)=" + ballh0);
+
 				// 値保存のためのフラグ
 				context.setGoalieFlag(true);
-				log.debug("GoalieFlag = 1");
-				log.debug("balld(after) =" + balld);
-				log.debug("balld0(after)=" + balld0);
-				log.debug("ballh(after) =" + ballh);
-				log.debug("ballh0(after)=" + ballh0);
+//				log.debug("GoalieFlag = 1");
 
 			}
 			// 連続でキックさせないためのフラグ
@@ -73,14 +75,10 @@ public class GoalieTask extends Task {
 			}
 
 		} else {
-			balld = 0;
-			ballh = 0;
-			balld0 = 0;
-			ballh0 = 0;
-			log.debug("balld(ballc=0)  =" + balld);
-			log.debug("balld0(ballc=0) =" + balld0);
-			log.debug("ballh(ballc=0)  =" + ballh);
-			log.debug("ballh0(ballc=0) =" + ballh0);
+//			balld = 0;
+//			ballh = 0;
+//			balld0 = 0;
+//			ballh0 = 0;
 
 		}
 	}
@@ -93,131 +91,120 @@ public class GoalieTask extends Task {
 	@Override
 	public void continueTask(StrategyContext context) {
 		// TODO 自動生成されたメソッド・スタブ
+
+		log.info("continue");
 		tracking.setMode(Mode.Cont);
 		WorldObject ball = context.getBall();
 		balld = ball.getDistance();
 		ballh = ball.getHeading();
 		ballc = ball.getConfidence();
 
-		if (balld0 != 0) {
+		if (context.getGoalieFlag() == true) {
 
-			if (context.getGoalieFlag() == true) {
+			context.setGoalieFlag(false);
+			log.info("GoalieFlag = 0");
 
-				context.setGoalieFlag(false);
-				log.info("GoalieFlag = 0");
+			log.info("balld  =" + balld);
+			log.info("balld0 =" + balld0);
+			log.info("ballh  =" + ballh);
+			log.info("ballh0 =" + ballh0);
 
-				log.info("balld  =" + balld);
-				log.info("balld0 =" + balld0);
-				log.info("ballh  =" + ballh);
-				log.info("ballh0 =" + ballh0);
-
-				if (balld < 820) {
-					// ballがこっちに向かっているかを判定
-					if (balld0 - balld >= 20) {
-						// ballが左側にあるとき
-						if (ballh0 > 10) {
-							// ballが左側からゴーリーの左側に抜けるとき
-							if (ballh0 < ballh) {
-								log.info("L^L");
-								context.makemotion(Motions.MOTION_L_GORI_ITO);
-							} else
-							// ballが左側からゴーリーの右側に抜けるとき
-							if (ballh0 > ballh) {
-								log.info("L^R");
-								context.makemotion(Motions.MOTION_R_GORI_ITO);
-							}
+			if (balld < 820) {
+				// ballがこっちに向かっているかを判定
+				if (balld0 - balld >= 10) {
+					// ballが左側にあるとき
+					if (ballh0 > 10) {
+						// ballが左側からゴーリーの左側に抜けるとき
+						if (ballh0 < ballh) {
+							log.info("L^L");
+							context.makemotion(Motions.MOTION_L_GORI_ITO);
 						} else
-						// ballが右側にあるとき
-						if (ballh0 < -10) {
-							// ballが右側からゴーリーの左側に抜けるとき
-							if (ballh0 < ballh) {
-								log.info("R^L");
-								context.makemotion(Motions.MOTION_L_GORI_ITO);
-
-							} else
-							// ballが右側からゴーリーの右側に抜けるとき
-							if (ballh0 > ballh) {
-								log.info("R^R");
-								context.makemotion(Motions.MOTION_R_GORI_ITO);
-
-							}
-						} else
-						// ballが正面にあるとき
-						if (balld < 180 && context.getGoalieKickFlag() == true) {
-							// Kickの使い分け
-							if (ballh > 0) {
-								if (ballh < 30) {
-									log.info("C^SL");
-									context.makemotion(Motions.MOTION_L_GOKI_ITO);
-								} else if (ballh >= 30 && ballh < 48) {
-									log.info("C^CL");
-									context.makemotion(Motions.MOTION_L_GOKIC_ITO);
-								}
-							} else if (ballh < 0) {
-								if (ballh > -30) {
-									log.info("C^SR");
-									context.makemotion(Motions.MOTION_R_GOKI_ITO);
-								} else if (ballh <= -30 && ballh > -48) {
-									log.info("C^CR");
-									context.makemotion(Motions.MOTION_R_GOKIC_ITO);
-								}
-							}
-
-							context.setGoalieKickFlag(false);
-							log.info("GoalieKickFlag=0");
+						// ballが左側からゴーリーの右側に抜けるとき
+						if (ballh0 > ballh) {
+							log.info("L^R");
+							context.makemotion(Motions.MOTION_R_GORI_ITO);
 						}
 					} else
+					// ballが右側にあるとき
+					if (ballh0 < -10) {
+						// ballが右側からゴーリーの左側に抜けるとき
+						if (ballh0 < ballh) {
+							log.info("R^L");
+							context.makemotion(Motions.MOTION_L_GORI_ITO);
 
-					// ballが止まってる時。
-					if (balld < 180 && context.getGoalieKickFlag() == true) {
+						} else
+						// ballが右側からゴーリーの右側に抜けるとき
+						if (ballh0 > ballh) {
+							log.info("R^R");
+							context.makemotion(Motions.MOTION_R_GORI_ITO);
+
+						}
+					} else
+					// ballが正面にあるとき
+					if (balld < 230 && context.getGoalieKickFlag() == true) {
 						// Kickの使い分け
 						if (ballh > 0) {
-							if (ballh < 30) {
-								log.info("S^C^SL");
+							if (ballh < 35) {
+								log.info("C^SL");
 								context.makemotion(Motions.MOTION_L_GOKI_ITO);
-							} else if (ballh >= 30 && ballh < 48) {
-								log.info("S^C^CL");
+							} else if (ballh >= 35 && ballh < 50) {
+								log.info("C^CL");
 								context.makemotion(Motions.MOTION_L_GOKIC_ITO);
-							} else if (ballh >= 48 && ballh < 70) {
-								log.info("L---------------------");
 							}
 						} else if (ballh < 0) {
-							if (ballh > -30) {
-								log.info("S^C^SR");
+							if (ballh > -35) {
+								log.info("C^SR");
 								context.makemotion(Motions.MOTION_R_GOKI_ITO);
-							} else if (ballh <= -30 && ballh > -48) {
-								log.info("S^C^CR");
+							} else if (ballh <= -35 && ballh > -50) {
+								log.info("C^CR");
 								context.makemotion(Motions.MOTION_R_GOKIC_ITO);
-							} else if (ballh < -48 && ballh > -70) {
-								log.info("R------------------------");
 							}
 						}
 
 						context.setGoalieKickFlag(false);
 						log.info("GoalieKickFlag=0");
 					}
-				}
-				// 前保存した値を初期化する
-				balld = 0;
-				ballh = 0;
-				balld0 = 0;
-				ballh0 = 0;
-				log.info("balld(zero) =" + balld);
-				log.info("ballh(zero) =" + ballh);
-				log.info("balld0(zero)=" + balld0);
-				log.info("ballh0(zero)=" + ballh0);
-			}
+				} else
 
+				// ballが止まってる時。
+				if (balld < 230 && context.getGoalieKickFlag() == true) {
+					// Kickの使い分け
+					if (ballh > 0) {
+						if (ballh < 35) {
+							log.info("S^C^SL");
+							context.makemotion(Motions.MOTION_L_GOKI_ITO);
+						} else if (ballh >= 35 && ballh < 50) {
+							log.info("S^C^CL");
+							context.makemotion(Motions.MOTION_L_GOKIC_ITO);
+						} else if (ballh >= 50 && ballh < 70) {
+							log.info("L---------------------");
+						}
+					} else if (ballh < 0) {
+						if (ballh > -35) {
+							log.info("S^C^SR");
+							context.makemotion(Motions.MOTION_R_GOKI_ITO);
+						} else if (ballh <= -35 && ballh > -50) {
+							log.info("S^C^CR");
+							context.makemotion(Motions.MOTION_R_GOKIC_ITO);
+						} else if (ballh <= -50 && ballh > -70) {
+							log.info("R------------------------");
+						}
+					}
+
+					context.setGoalieKickFlag(false);
+					log.info("GoalieKickFlag=0");
+				}
+			}
+			// // 前保存した値を初期化する
+			// balld = 0;
+			// ballh = 0;
+			// balld0 = 0;
+			// ballh0 = 0;
+			// log.info("balld(zero) =" + balld);
+			// log.info("ballh(zero) =" + ballh);
+			// log.info("balld0(zero)=" + balld0);
+			// log.info("ballh0(zero)=" + ballh0);
 		}
-		// 前保存した値を初期化する
-		balld = 0;
-		ballh = 0;
-		balld0 = 0;
-		ballh0 = 0;
-		log.info("balld(zero1) =" + balld);
-		log.info("ballh(zero1) =" + ballh);
-		log.info("balld0(zero1)=" + balld0);
-		log.info("ballh0(zero1)=" + ballh0);
 	}// continue end
 
 	@Override
